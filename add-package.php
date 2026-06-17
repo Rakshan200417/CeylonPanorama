e120dadbda8f8e65153c7900a0b70a5793ead693
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:33:28 +0530

    Add responsive CSS link to add-package.php
    Added responsive CSS for better layout on different devices.

--- add-package.php
+++ add-package.php
@@ -57,6 +57,7 @@ if (isset($_POST['add_package'])) {
 <title>Add Package</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body class="bg-light">
 <!-- NAVBAR PLACEHOLDER: Paste your navbar here -->
