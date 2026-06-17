8168616c1fe9639ce0921f4b11b265374c1c1f78
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:31:18 +0530

    Update payment_success.php

--- payment_success.php
+++ payment_success.php
@@ -31,6 +31,7 @@ $stmt->close();
     <meta charset="UTF-8">
     <title>Payment Success</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/responsive.css">
     <style>
         body {
             background-color: #f4f4f9;
