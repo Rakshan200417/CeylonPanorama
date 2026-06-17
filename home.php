061de517d41f59ea6d9117fdb832ba2c8abb0230
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:00:13 +0530

    Update home.php

--- home.php
+++ home.php
@@ -15,7 +15,7 @@ include 'db.php';
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-
+  <link rel="stylesheet" href="css/responsive.css">
   <!-- Custom CSS -->
   <link rel="stylesheet" href="css/home.css">
 </head>
