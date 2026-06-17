9559ffd2f968991dbce2a4ddebacc4bb8dfbf13b
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:35:05 +0530

    Update update-package.php

--- update-package.php
+++ update-package.php
@@ -72,6 +72,7 @@ if (isset($_POST['update_package'])) {
 <title>Update Package</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body class="bg-light">
 <!-- NAVBAR PLACEHOLDER -->
