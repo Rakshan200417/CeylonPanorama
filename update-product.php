a12321bf4001bd0a6c124346296453d4294e3b0e
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:35:29 +0530

    Update update-product.php

--- update-product.php
+++ update-product.php
@@ -66,6 +66,7 @@ if (isset($_POST['update_product'])) {
 <meta charset="UTF-8">
 <title>Update Product</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body class="bg-light">
 <!-- NAVBAR PLACEHOLDER -->
