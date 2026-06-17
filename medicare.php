dc1093d09de215b13bf5c0c057a52f6e9c61f52d
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:30:30 +0530

    Fix HTML encoding issues in medicare.php

--- medicare.php
+++ medicare.php
@@ -43,6 +43,7 @@ $conn->close();
     <title>Medicare Booking - Ceylon Panorama</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="medicare.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body>
 
