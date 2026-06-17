2216b839748da5dedcc6a6272f1da60d8555847b
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:32:40 +0530

    Fix HTML entities and improve form structure

--- transport.php
+++ transport.php
@@ -44,6 +44,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Transport Booking - Ceylon Panorama</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="transport.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 
 <body>
