4cf813c64fdca53cf9f7ba9ecaf2ac7e54104ad8
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:34:37 +0530

    Update admin_signup.php

--- admin_signup.php
+++ admin_signup.php
@@ -52,6 +52,7 @@ $redirect = isset($_GET['redirect']) ? $_GET['redirect'] : 'home.php';
     <title>Admin Signup</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="adminsignup.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body>
 <div class="signup-box">
