1f6a8510fda1cc7a82d05181b5d76406a77ac318
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:29:14 +0530

    Include responsive CSS in payment services page
    Added a link to the responsive CSS file for improved styling.

--- fakepaymentservices.php
+++ fakepaymentservices.php
@@ -9,6 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/responsive.css">
     <meta charset="UTF-8">
     <title>Payment Gateway</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
