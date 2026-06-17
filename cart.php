0142178786ea7fc32cb2b8c998ca76ed94787c1b
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 15:10:14 +0530

    Refactor cart.php for improved readability and structure

--- cart.php
+++ cart.php
@@ -272,6 +272,7 @@ $stmt_items->close();
             </div>
         <?php endif; ?>
     </div>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
