7a515ffdcbac994600444003575effced46bc1e5
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:23:56 +0530

    Include responsive CSS for hotel booking page
    Added responsive CSS for better layout.

--- hotel.php
+++ hotel.php
@@ -64,6 +64,7 @@ $conn->close();
     <title>Hotel Booking - Ceylon Panorama</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="hotel.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 
 <body>
