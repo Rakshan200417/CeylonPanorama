d9f26c04a81ae5c2e139d01953f1b6c005b17396
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:26:08 +0530

    Refactor signin.php for improved structure and style

--- signin.php
+++ signin.php
@@ -54,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <title>Sign In | Ceylon Panorama</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="auth.css">
+    <link rel="stylesheet" href="css/responsive.css">
 <script>
 function toggleForm(type) {
     if(type === 'user'){
