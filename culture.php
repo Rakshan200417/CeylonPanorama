0490a623b1b09c38cbc70425144207ec62ae7267
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:28:52 +0530

    Update culture.php

--- culture.php
+++ culture.php
@@ -21,6 +21,7 @@ while($row = $packagesResult->fetch_assoc()){
     <title>Ceylon Panorama | Culture Packages</title>
     <link rel="stylesheet" href="css/culture.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body>
 
