efb29fcf4c5196860fc544b2c280a40acd54cf45
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:32:16 +0530

    Update tourguide.php

--- tourguide.php
+++ tourguide.php
@@ -40,6 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Tour Guide Booking - Ceylon Panorama</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="tourguide.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body>
 
