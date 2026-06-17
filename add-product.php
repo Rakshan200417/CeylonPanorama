547a16c2c5426ee0614fb8aa4de543d6a164285e
Rakshan200417 <kukarakshan2004@gmail.com>
2026-06-17 14:33:55 +0530

    Add responsive CSS link to add-product.php
    Added responsive CSS for better layout on mobile devices.

--- add-product.php
+++ add-product.php
@@ -52,6 +52,7 @@ if (isset($_POST['add_product'])) {
 <title>Add Product</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body class="bg-light">
 <!-- NAVBAR PLACEHOLDER -->
